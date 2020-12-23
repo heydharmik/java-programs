cc122e61e01472298b314929e53bb78296e31cec
Dharmik <dharmikpajwanigithub@gmail.com>
2020-12-23 14:17:17 +0530

    Initial commit and doing it by committing four files at a go

---
+++ AreaOfCircleWithoutUsingScanner.java
@@ -0,0 +1,11 @@
+
+public class AreaOfCircleWithoutUsingScanner {
+
+	public static void main(String[] args) {
+		final double PI = 3.1417;
+		int radius=4;
+		
+		System.out.println("Area of circle is: "+((radius*radius)*PI));
+	}
+
+}
---
+++ ScannerClassExample.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScannerClassExample {
+	
+	
+	public static void main(String[] args) {
+		Scanner scan;
+		String name;
+		System.out.print("Enter your name: ");
+		scan = new Scanner(System.in);
+		name = scan.next();
+		System.out.println("Welcome "+name+" to your first program.");
+		scan.close();
+	}
+
+}
