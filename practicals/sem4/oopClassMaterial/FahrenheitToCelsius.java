08f6a424903990307b33782e3a5d10ad218594d2
Dharmik <dharmikpajwanigithub@gmail.com>
2020-12-30 14:58:55 +0530

    Kelvin login added in the program and Monetary Units program created

--- practicals/sem4/oopClassMaterial/FahrenheitToCelsius.java
+++ practicals/sem4/oopClassMaterial/FahrenheitToCelsius.java
@@ -7,14 +7,18 @@ public class FahrenheitToCelsius {
 		Scanner input;
 		double fahrenheit;
 		double celsius;
+		double kelvin;
 		
 		System.out.println("Enter a degree in Fahrenheit: ");
 		input = new Scanner(System.in);
 		fahrenheit = input.nextDouble();
 		
 		celsius = (5.0/9) * (fahrenheit-32);
+		kelvin = celsius + 273.15;
 		
 		System.out.println("Temperature in Celsius is: " + celsius + "°C");
+		System.out.println("Temperature in Kelvin is: " + kelvin + "°K");
+		
 		input.close();
 	}
 }
---
+++ practicals/sem4/oopClassMaterial/MonetaryUnits.java
@@ -0,0 +1,16 @@
+package practicals.sem4.oopClassMaterial;
+
+import java.util.Scanner;
+
+public class MonetaryUnits {
+	public static void main(String[] args) {
+		Scanner input;
+		double dollars;
+		double cents;
+		
+		double quarters, dimes, nickel, pennies;
+		
+		System.out.println("Do you want to enter amount in Dollars or cents?");
+		System.out.println("Press ");
+	}
+}
