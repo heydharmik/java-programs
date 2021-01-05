4a870b87e07843927dafa3321d176f35f65caf55
Dharmik <dharmikpajwanigithub@gmail.com>
2021-01-05 14:24:58 +0530

    Leap year program done

---
+++ practicals/sem4/oopClassMaterial/LeapYear.java
@@ -0,0 +1,42 @@
+package practicals.sem4.oopClassMaterial;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		int year;
+		Scanner input;
+	    boolean leap = false;
+	    input = new Scanner(System.in);
+	    year = input.nextInt();
+
+	    // if the year is divided by 4
+	    if (year % 4 == 0) {
+
+	      // if the year is century
+	      if (year % 100 == 0) {
+
+	        // if year is divided by 400
+	        // then it is a leap year
+	        if (year % 400 == 0)
+	          leap = true;
+	        else
+	          leap = false;
+	      }
+	      
+	      // if the year is not century
+	      else
+	        leap = true;
+	    }
+	    
+	    else
+	      leap = false;
+
+	    if (leap)
+	      System.out.println(year + " is a leap year.");
+	    else
+	      System.out.println(year + " is not a leap year.");
+	    
+	    input.close();
+	}
+}
