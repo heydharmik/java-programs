ba535cd654fab6758dc3211148a221f284bf0fb4
Dharmik <dharmikpajwanigithub@gmail.com>
2021-01-06 14:45:36 +0530

    Reduced duplication of code.

--- practicals/sem4/sessionPracticals/TenNumberPlatesGeneration.java
+++ practicals/sem4/sessionPracticals/TenNumberPlatesGeneration.java
@@ -5,7 +5,7 @@ public class TenNumberPlatesGeneration {
 		System.out.println("Ten Number plates are:");
 		for(int i = 0; i < 10;) {
 			int ran;
-			ran = (int) Math.round(Math.abs(Math.random()*10000)); 
+			ran = (int) Math.abs(Math.random()*10000); 
 			if(ran >= 1000 && ran <= 10000) {
 				System.out.println("AGM-" + ran);
 				i++;
